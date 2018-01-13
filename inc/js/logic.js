3c1965c6b095291e0d308778506559c264442e5c
trimkas <34684294+trimkas@users.noreply.github.com>
2018-01-13 02:23:29 +0100

    first usable form, without filters

--- inc/js/logic.js
+++ inc/js/logic.js
@@ -1,7 +1,31 @@
 
-function addTask(){
+
+	function addTask(){
+
+			//to avoid empty todo's
+			if(task != ""){
+
+				//a variable to get what's being inputted 
+				var task = $('#inputtask').val();
+
+				//add an li element with checkbox to the list, with the task from input as text
+				$('#tabela').append('<li><input type="checkbox">' + task + '</li>');
+				
+				// declare this, so it only strikes the unique li, not all of them
+				$('input:checkbox').click(function(){
+					var $this = $(this);
+					if (this.checked){
+						$this.parent().addClass('done');
+					}else{
+						$this.parent().removeClass('done');
+					}
+
+				});
+
+			}
+
 		
-		var task = $('#inputtask').val();
-		$('#tabela').append(task + "<br>");
-	}
+			//clears the input
+			$('#inputtask').val('');
+		}
 
