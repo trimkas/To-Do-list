3eaf8498010641daff39a0c549f93a3104c8b4f8
trimkas <34684294+trimkas@users.noreply.github.com>
2018-01-14 04:50:57 +0100

    added filters, added Enter to submit

--- inc/js/logic.js
+++ inc/js/logic.js
@@ -1,31 +1,65 @@
+	$( document ).ready(function() {
+		//maps enter when document is ready   		
+		mapEnter();
 
+	});
+	
+
+	//listens for keypress nr 13, which is ascii for enter
+	function mapEnter(){
+		$("#inputtask").on("keypress", function (e){
+			if(e.which === 13) {
+				addTask();
+			}
+		}) 
+
+	}
 
 	function addTask(){
+
+		//a variable to get what's being inputted 
+		var task = $('#inputtask').val();
 
 			//to avoid empty todo's
 			if(task != ""){
 
-				//a variable to get what's being inputted 
-				var task = $('#inputtask').val();
-
 				//add an li element with checkbox to the list, with the task from input as text
-				$('#tabela').append('<li><input type="checkbox">' + task + '</li>');
+				$('#tabela').append('<li class = "item"><input type="checkbox">' + task + '</li>');
 				
 				// declare this, so it only strikes the unique li, not all of them
 				$('input:checkbox').click(function(){
 					var $this = $(this);
 					if (this.checked){
+						$this.parent().removeClass();
 						$this.parent().addClass('done');
 					}else{
 						$this.parent().removeClass('done');
+						$this.parent().addClass('item')
 					}
-
 				});
 
 			}
 
-		
 			//clears the input
 			$('#inputtask').val('');
+		}
+
+
+
+		function filterTodo(){
+
+			$("li").hide();
+			$("li.item").show();
+		}
+
+		function filterDone(){
+
+			$("li").hide();
+			$("li.done").show();
+		}
+
+		
+		function filterAll(){
+			$("li").show();
 		}
 
--- readme.md
+++ readme.md
@@ -1,11 +1,14 @@
 A simple to do list.
 
-Features:
+Change log:
 -adding tasks
 -marking them done
+-filter to do / doing tasks (spent an hour looking at this problem, needed to only change one letter of code)
+-map enter key to add task
 
 To be added:
--filter tasks based on state
--map enter key to add task
--map buttons to filter todo's
+-no tasks alerts
+-highlight active filter (css)
+-add number of todo items
+-add X to delete tasks
 -make it look a bit better because it looks like something from 2002
